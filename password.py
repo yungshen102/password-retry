49b20be1df39009cf237f09cb37e25e520ae9b5e
Sam <nqbviwdpm@gmail.com>
2023-01-04 12:32:48 +0800

    change to while i > 0

--- password.py
+++ password.py
@@ -5,8 +5,8 @@
 # 如果正確 就印出 "登入成功!"
 # 如果不正確 就印出 "密碼錯誤! 還有__次機會!"
 password = 'a123456'
-i = 3
-while True:
+i = 3 # 剩餘機會
+while i > 0:
 	pwd = input('請輸入密碼： ')
 	if pwd == password:
 		print('登入成功!')
@@ -14,6 +14,4 @@ while True:
 	else:
 		i = i - 1
 		print('密碼錯誤! 還有', i, '次機會')
-		if i == 0:
-			break
-
+		
